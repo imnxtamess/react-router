b7a1d88e2cec4c1b2749c973074421c913e0f11b
imnxtamess <davide.morisco.work@gmail.com>
2025-04-01 17:38:47 +0200

    Enhance Posts component layout and styling: add card structure for posts with headers and images

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,26 @@
 import posts from "../data/posts";
 
 export default function Posts() {
-  return <h1>Posts</h1>;
+  return (
+    <main>
+      <section>
+        <div className="container my-5">
+          <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-5">
+            {posts.map((post) => (
+              <div className="col">
+                <div className="card">
+                  <div className="card-header">
+                    <h3>{post.title}</h3>
+                  </div>
+                  <div className="card-img-top ">
+                    <img src={post.image} alt="" />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
