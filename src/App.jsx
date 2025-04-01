f5d4a0ada952df2f356977e3fc7d111b2e71bb24
imnxtamess <davide.morisco.work@gmail.com>
2025-04-01 12:56:55 +0200

    Implement routing and layout structure with Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Posts from "./pages/Posts";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={Home} />
+            <Route path="/About-Us" Component={AboutUs} />
+            <Route path="/Posts" Component={Posts} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
