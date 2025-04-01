4a2a0ccec06dac59bba0a956464671f1f58c8707
imnxtamess <davide.morisco.work@gmail.com>
2025-04-01 14:19:39 +0200

    Add Header and Footer components with basic layout structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              LOGO
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+                earum ex exercitationem veniam autem ratione corrupti cupiditate
+                obcaecati, asperiores reiciendis eum, iure possimus est dicta
+                esse aliquam mollitia eos veritatis?
+              </p>
+            </div>
+            <div className="col">
+              <h3>lorem</h3>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              <h3>lorem</h3>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+                <li>
+                  <a href="">lorem</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
